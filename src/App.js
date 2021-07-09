394b46cd27a6f8d3016ac869afbe69cd736b8b1b
Ivan <ivanpronin1379@gmail.com>
2021-07-09 17:26:23 +0500

    фиксация ("master")

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Мой первый проект на React
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/impossibleplayer"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Мой профиль GitHub
         </a>
       </header>
     </div>
